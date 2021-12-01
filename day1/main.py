ecbbc63275ee33131af49aaac22169ea824e9ff8
Jonas Lundin <31651327+JonasLundin@users.noreply.github.com>
2021-12-01 19:08:14 +0100

    Fix shit code

--- day1/main.py
+++ day1/main.py
@@ -1,26 +1,28 @@
 my_file = open("input.txt", "r")
 content = my_file.read()
 content_list = content.split()
+numbers = [ int(x) for x in content_list ]
 
 def part1():
-    k = 1
+    k = 0
 
-    for i, j in enumerate(content_list):
-        if j > content_list[i - 1]:
+    for i, j in enumerate(numbers):
+        if j > numbers[i - 1]:
             k += 1  
     print(f"Part1: {k}")
 
 def part2():
     k = 0
 
-    for i, j in enumerate(content_list):
-        if i == len(content_list) - 3:
+    for i, j in enumerate(numbers):
+        if i == len(numbers) - 3:
             break
-        a = content_list[i] + content_list[i + 1] + content_list[i + 2]
-        b = content_list[i + 1] + content_list[i + 2] + content_list[i + 3]
+        a = numbers[i] + numbers[i + 1] + numbers[i + 2]
+        b = numbers[i + 1] + numbers[i + 2] + numbers[i + 3]
         if b > a:
             k += 1 
 
     print(f"Part2: {k}")
 part1()
 part2()
+
