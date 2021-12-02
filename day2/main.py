f479e179843c69f3cbf2d5a6e5c8344698f1cd57
Jonas Lundin <jonas.se.lundin@gmail.com>
2021-12-02 09:16:10 +0100

    Day 2 finished

--- day2/main.py
+++ day2/main.py
@@ -1,18 +1,38 @@
 with open("input.txt") as load_file:
     data = [tuple(line.split()) for line in load_file]
 
-depth = 0
-horizonal_position = 0
+def part1():
+    depth = 0
+    horizonal_position = 0
 
-for i, tuple in enumerate(data):
-    command = tuple[0]
-    value = tuple[1]
-    print(command,value)
-    if command == "down":
-        depth = depth + int(value)
-    elif command == "up":
-        depth = depth - int(value)
-    else:
-        horizonal_position = horizonal_position + int(value)
-print(f"Depth: {depth}. Horizontal position {horizonal_position}")
-print(f"Result: {depth * horizonal_position}.")
+    for i, tuple in enumerate(data):
+        command = tuple[0]
+        value = tuple[1]
+        if command == "down":
+            depth = depth + int(value)
+        elif command == "up":
+            depth = depth - int(value)
+        else:
+            horizonal_position = horizonal_position + int(value)
+    print(f"Depth: {depth}. Horizontal position {horizonal_position}")
+    print(f"Result: {depth * horizonal_position}.")
+
+def part2():
+    depth = 0
+    horizonal_position = 0
+    aim = 0
+
+    for i, tuple in enumerate(data):
+        command = tuple[0]
+        value = tuple[1]
+        if command == "down":
+            aim = aim + int(value)
+        elif command == "up":
+            aim = aim - int(value)
+        else:
+            horizonal_position = horizonal_position + int(value)
+            depth = depth + aim*int(value)
+    print(f"Depth: {depth}. Horizontal position {horizonal_position}")
+    print(f"Result: {depth * horizonal_position}.")
+part1()
+part2()
